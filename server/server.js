02879ef9aa97b1e8cc93f352875ea4ab25f8beb8
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-11-07 13:53:59 +0200

    understnding how to downlod image

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const request = require('request');
 
 const app = express();
 const router = express.Router();
@@ -7,11 +9,23 @@ const router = express.Router();
 const port = process.env.API_PORT || 3001;
 mongoose.connect('mongodb://god:blessrng7@ds253783.mlab.com:53783/images',  {useNewUrlParser: true});
 
-function sendError(res) {
-  return function (e) {
-    return res.status(500).send(e);
-  };
-}
+// function sendError(res) {
+//   return function (e) {
+//     return res.status(500).send(e);
+//   };
+// }
+let download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download('http://www.whelanslive.com/wp-content/uploads/2018/06/JMITP-Logo-image-p.jpg', './public/images/google.png', function(){
+  console.log('done');
+});
 
 app.use('/api', router);
 
